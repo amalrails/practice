6c17c0846b9799c5712258946f98a15f2efa1457
amalrails <69242143+amalrails@users.noreply.github.com>
2021-03-01 13:06:01 +0000

    Delete two sums directory

--- two sums/two_sum.rb
+++
@@ -1,9 +0,0 @@
-def two_sum(arr, sum)
-	arr_hash = {}
-	arr.each_with_index { |item, index| arr_hash[:ele] = index } # o(n)
-	arr.each_with_index do |item, index| # o(n)
-		number = sum - item
-		return [index, arr_hash[:ele]] if arr_hash[:number]
-	end
-	p 'no elements summing up'
-end
